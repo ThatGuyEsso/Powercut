4c93a84c50de55f9d57725e1e1619352cb5e99f6
Esewi Egheosa <esewi12@gmail.com>
2021-04-08 14:19:35 +0000

    Boss health bar added, and boss ability skeleton classes set up

---
+++ Assets/Scripts/UX/UI/Sliders&Counters/ScalingProgressBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScalingProgressBar : MonoBehaviour
+{
+    [SerializeField] private Image fill;
+    [SerializeField] private float maxValue;
+    [SerializeField] private float value;
+
+    public void SetMaxValue(float maxVal) { maxValue = maxVal; }
+    public void SetValue(float newVal) { value = newVal; }
+
+    public void UpdateValue(float newVal)
+    {
+        SetValue(newVal);
+    }
+}
