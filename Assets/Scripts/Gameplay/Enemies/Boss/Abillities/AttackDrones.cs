6c60b0617eb29fd19b8a9b619b4865a0104d638a
Esewi Egheosa <esewi12@gmail.com>
2021-04-09 14:24:25 +0000

    Implemented abstract classes

--- Assets/Scripts/Gameplay/Enemies/Boss/Abillities/AttackDrones.cs
+++ Assets/Scripts/Gameplay/Enemies/Boss/Abillities/AttackDrones.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class AttackDrones : BaseAttackPattern
 {
+    protected override void ExecuteAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Gameplay/Enemies/Boss/Abillities/PheremoneBlast.cs
+++ Assets/Scripts/Gameplay/Enemies/Boss/Abillities/PheremoneBlast.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PheremoneBlast : BaseAttackPattern 
+public class PheremoneBlast : BaseAttackPattern
 {
- 
+    protected override void ExecuteAttack()
+    {
+        throw new System.NotImplementedException();
+    }
 }
